2f3d69bb6e90116239c3a97e82ae222c77b435f8
Siddharth Basu <uditabasu17@gmail.com>
2026-01-17 06:25:27 +0000

    Code changed

--- java-jenkins-demo/src/main/java/com/example/demo/HelloController.java
+++ java-jenkins-demo/src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Jenkins CI/CD";
+        return "My first deployment using CI-CD Pipeline! Lets hope for the best";
     }
 }
