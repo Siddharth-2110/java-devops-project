0534db22d3d4c91ed190c1f0a7f70309c08db25c
Siddharth Basu <uditabasu17@gmail.com>
2026-05-08 07:52:21 +0000

    Indian Standard Time included in deployment page

--- java-jenkins-demo/src/main/java/com/example/demo/HelloController.java
+++ java-jenkins-demo/src/main/java/com/example/demo/HelloController.java
@@ -3,27 +3,28 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class HelloController {
 
-    // This increments once every time the class is loaded (i.e. recompile/restart)
-    private static final AtomicInteger DEPLOY_COUNTER = new AtomicInteger(1);
+	    private static final AtomicInteger DEPLOY_COUNTER = new AtomicInteger(1);
+	        private static final int CURRENT_DEPLOYMENT = DEPLOY_COUNTER.getAndIncrement();
 
-    // Capture deployment count at startup
-    private static final int CURRENT_DEPLOYMENT = DEPLOY_COUNTER.getAndIncrement();
+		    private static final ZoneId IST_ZONE = ZoneId.of("Asia/Kolkata");
+		        private static final DateTimeFormatter FORMATTER =
+				            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
 
-    @GetMapping("/")
-    public String hello() {
-        String currentTime = LocalDateTime.now()
-                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+			    @GetMapping("/")
+			        public String hello() {
+					        String currentTime = ZonedDateTime.now(IST_ZONE).format(FORMATTER);
 
-        return "My first deployment using CI-CD Pipeline! "
-                + "Deployment #" + CURRENT_DEPLOYMENT
-                + " | Time: " + currentTime + " This is my application 1 final built with Docker Compose " + "May 2026";
-    }
+						        return "My first deployment using CI-CD Pipeline! "
+								                + "Deployment #" + CURRENT_DEPLOYMENT
+										                + " | Time: " + currentTime
+												                + " | This is my application 1 final built with Docker Compose | May 2026";
+							    }
 }
-
